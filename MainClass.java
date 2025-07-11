6a76fa57313857dcc8b8efdf9a13ce8072755949
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-12 02:43:46 +0530

    Add MainClass implementation with main method for parsing and printing an integer

---
+++ MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]); // Parses the first argument as an int
+        System.out.println("First Number: " + a); // Prints the number
+    }
+}
