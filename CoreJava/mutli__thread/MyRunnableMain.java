3b4d6182b58fc2ac2fedee6b0e66e54fbb08ad13
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-30 02:48:43 +0530

    implement : join ()

--- CoreJava/mutli__thread/MyRunnableMain.java
+++ CoreJava/mutli__thread/MyRunnableMain.java
@@ -2,9 +2,30 @@ package mutli__thread;
 
 public class MyRunnableMain {
     public static void main(String[] args) {
+        
         MyRunnableThread thread=new MyRunnableThread();
         Thread thread1=new Thread(thread);
+        thread1.setName("MyRunnableThread-1");
         thread1.start();
+
+
+
+        Thread thread2=new Thread(thread, "MyRunnableThread-2");
+        Thread threadName=Thread.currentThread();
+        thread2.start();
+        for(int i=1;i<=10;i++){
+            System.out.println(threadName.getName()+" : "+i);
+        }
+
+        
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("End of the Application !!!!");
     }
 
 }
--- CoreJava/mutli__thread/MyRunnableThread.java
+++ CoreJava/mutli__thread/MyRunnableThread.java
@@ -1,11 +1,20 @@
 package mutli__thread;
 
 public class MyRunnableThread implements Runnable{
+
+    
+
     @Override
     public void run()
     {
+        Thread threadName=Thread.currentThread();
         for(int i=1;i<=10;i++){
-            System.out.println(i);
+            System.out.println(threadName.getName()+" "+i);
         }
+    }
+
+    public void join() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'join'");
     }
 }
