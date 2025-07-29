0df8e7d419cebe21b36178cf481ef60fd1507e4a
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-29 21:14:15 +0530

    hash set example

--- CoreJava/collection/hashset/HashSetExample.java
+++ CoreJava/collection/hashset/HashSetExample.java
@@ -24,6 +24,24 @@ public class HashSetExample
             System.out.println(iterator.next()+" ");    
         }
 
+        HashSet <String> hashSet2=new HashSet<>();
+        hashSet2.add("Hello");
+        hashSet2.add("Bye");
+        hashSet2.add("bye");
+        hashSet2.add("Hello");
+        hashSet2.add("neha");
+        hashSet2.add("bye");
+        hashSet2.add("Bye");
+
+
+        System.out.println("HashSet Element : ");
+        System.out.println(" String HashSet Element  : "+ hashSet2);
+
+        Iterator <String> iterator2=hashSet2.iterator();
+        while (iterator2.hasNext())
+        {
+            System.out.println(iterator2.next()+" ");
+        }
 
     }
     
