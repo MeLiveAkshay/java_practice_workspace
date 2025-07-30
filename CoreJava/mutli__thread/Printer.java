f32011f7db1a5ca0c5b04094786fa82bf327add3
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-07-30 11:04:28 +0530

    synchronized block

--- CoreJava/mutli__thread/Printer.java
+++ CoreJava/mutli__thread/Printer.java
@@ -4,9 +4,11 @@ public class Printer {
     //synchronization method
     public  synchronized void print(String msg) throws InterruptedException
     {
-        System.out.print(" [ "+ msg);
-        Thread.sleep(1000);
-        System.out.println(" ] ");
+        synchronized (this){
+            System.out.print(" [ "+ msg);
+            Thread.sleep(1000);
+            System.out.println(" ] ");
+        }
     }
 
 
