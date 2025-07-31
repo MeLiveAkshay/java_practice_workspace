0bbb6572d9333fca2cd239024d19bfdb9244f3e0
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-08-01 00:19:44 +0530

    complete:  Shared Resouce has been complete

--- CoreJava/mutli__thread/GetterThread.java
+++ CoreJava/mutli__thread/GetterThread.java
@@ -15,7 +15,12 @@ public class GetterThread implements Runnable {
     @Override
     public void run() {
         for(int i=0;i<11;i++){
-            resource.getNumber();
+            try {
+                resource.getNumber();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         
     }
--- CoreJava/mutli__thread/SetterThread.java
+++ CoreJava/mutli__thread/SetterThread.java
@@ -14,7 +14,12 @@ public class SetterThread implements Runnable  {
     @Override
     public void run() {
         for(int i=0;i<11;i++){
-            resource.setNumber(i);
+            try {
+                resource.setNumber(i);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
     
