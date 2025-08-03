2ca91c9b870f717b819d1151e35f375539cae3ad
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-08-03 17:13:36 +0530

    testing : check java code is run

--- mock_interview/mock_interview__9/TreeMapMock.java
+++ mock_interview/mock_interview__9/TreeMapMock.java
@@ -1,21 +1,26 @@
 package mock_interview.mock_interview__9;
 
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.Map;
 
 public class TreeMapMock {
     public static void main(String[] args) {
-        TreeMap<Integer, String >map = new TreeMap();
+        TreeMap<Integer, String> map = new TreeMap<>();
         map.put(1, "Akshay");
-        map.put(2,"Ajay");
-        map.put(3,"Vivek");
-        map.put(4,"Sumit");
+        map.put(2, "Ajay");
+        map.put(3, "Vivek");
+        map.put(4, "Sumit");
+        map.put(6, "Rahul");
+        map.put(7, "Sneha");
+        map.put(8, "Neha");
+        map.put(9, "Suresh");
 
-        System.out.println(map);
-        System.out.println(map.reversed());
+        System.out.println("Original Map: " + map);
 
-        
         
     }
+
+    
 }
